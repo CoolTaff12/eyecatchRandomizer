8025438754ba8a9659618d5d1a39730f689730c6
Leo Taffazoli <cool_taff@yahoo.com>
2023-05-25 07:39:06 +0200

    Patch 1
    Added many eyecatch examples.

--- main.py
+++ main.py
@@ -4,15 +4,17 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 # ----------------
-# from io import BytesIO
-# import numpy as np
-# from random import *
+from os import listdir
+from os.path import isfile, join
+import random
+import time
 # ----------------
 # vlc name is python-vlc if you want to install it
 import vlc
 # In this case libvlc.dll will not be found. Make sure to install the 64bit-version
 # ----------------
 
+mypath = ['Eyecatch_in/', 'Eyecatch_out/']
 
 # Runs in Python 3.8.
 # Package               version
@@ -23,20 +25,60 @@ import vlc
 # python-vlc (vlc)      30.0.18122
 
 
+def eyecatch_in():
+    onlyfiles1 = [f for f in listdir(mypath[0]) if isfile(join(mypath[0], f))]
+    onlyfiles2 = [f for f in listdir(mypath[1]) if isfile(join(mypath[1], f))]
+    random.shuffle(onlyfiles1)
+    random.shuffle(onlyfiles2)
+
+    # creating a media player object
+    media_player = vlc.MediaListPlayer()
+
+    # creating Instance class object
+    player = vlc.Instance()
+
+    # creating a new media list object
+    media_list = player.media_list_new()
+
+
+    # creating a new media
+    media = player.media_new(mypath[0] + str(onlyfiles1[0]))
+
+    # adding media to media list
+    media_list.add_media(media)
+
+    # setting media list to the media player
+    media_player.set_media_list(media_list)
+
+    # creating a new media
+    media = player.media_new(mypath[1] + str(onlyfiles2[0]))
+
+    # adding media to media list
+    media_list.add_media(media)
+
+    # setting media list to the media player
+    media_player.set_media_list(media_list)
+
+    # start playing video
+    media_player.play_item_at_index(0)
+    time.sleep(0.5)
+    time.sleep(media_list[0].get_duration() / 1000 + 0.2)
+
+    # playing next media in list
+    media_player.next()
+
+    # wait so the video can be played for 5 seconds
+    # irrespective for length of video
+    time.sleep(media_list[1].get_duration() / 1000 + 0.2)
+    # preloaded
+
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    eyecatch_in()
 
-
-def eyecatch_p1(id):
-    file = {
-        1: ["mPw6ecM33QzSi48Y.mp3", 0.1],
-        2: ["burunyaa.wav", 10.1],
-        3: ["suicide_eng.wav", 10.1],
-        4: ["Laughs_in_muda.mp3", 10.1]
-    }
-    play_the_fanfare = vlc.MediaPlayer('SFX/'+str(file.get(id)[0]))
-    play_the_fanfare.play()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
